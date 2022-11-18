0455002c2c745d86c6d15157cb217a1aa4655be4
Aula Jazmati <37100066+aula9@users.noreply.github.com>
2022-11-18 04:04:15 -0800

    Add files via upload

---
+++ UART_pico_Test1.py
@@ -0,0 +1,29 @@
+#AulaJazmati
+#Test
+#Hexabitz Modules with Raspberry Pi Pico Test
+from machine import UART, Pin
+import time
+uart = UART(0, baudrate= 921600, tx=Pin(0), rx=Pin(1))                         # init with given baudrate
+uart.init(921600, bits=8, parity=None, stop=1) # init with given parameters
+print('-- UART Serial --')
+print('>', end='')
+txData = b'\r'
+uart.write(txData)
+time.sleep(4)
+rxData = bytes()
+time.sleep(4)
+txData = b'on 90 \r'
+uart.write(txData.decode('utf-8'))
+time.sleep(4)
+while uart.any() > 0:
+    rxData = uart.readline()
+    print(rxData)
+    txData = b'color red 90 \r'
+    uart.write(txData.decode('utf-8'))
+    time.sleep(3)
+    txData = b'color green 90 \r'
+    uart.write(txData.decode('utf-8'))
+    time.sleep(3)
+    txData = b'color yellow 90 \r'
+    uart.write(txData.decode('utf-8'))
+    time.sleep(3)
